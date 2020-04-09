2df44fa0ccb33e5de8c0434baa051f7a39ca5b10
Anton <arnellomutty2@gmail.com>
2020-04-09 17:30:14 +0300

    Code refactor

--- components/Services/OtherServices.tsx
+++ components/Services/OtherServices.tsx
@@ -1,23 +1,13 @@
 import React from "react";
-import { makeStyles, createStyles, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import ServicesTitle from "./ServicesTitle";
 import { otherServices, IOtherServices } from "../../constants/otherServices";
 import ListItem from "./ListItem";
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    root: {
-      // padding: "10% 0 8%",
-    },
-    title: {
-      marginBottom: "2rem",
-    },
-  })
-);
+
 interface IProps {}
 
 export default function OtherServices(): React.ReactElement<IProps> {
-  const classes = useStyles();
   const renderService = (service: IOtherServices) => (
     <React.Fragment>
       <ServicesTitle text={service.title} />
@@ -27,7 +17,7 @@ export default function OtherServices(): React.ReactElement<IProps> {
     </React.Fragment>
   );
   return (
-    <div className={classes.root}>
+    <div>
       <Grid container spacing={2}>
         {otherServices.map((service) => (
           <Grid item sm={4} key={service.title}>
